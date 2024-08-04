5f4fc815f2331c516583a03fb1dd3c838b801be8
Meissa <meissasy@gmail.com>
2024-08-04 11:41:56 -0400

    append both divs into second-img class

--- src/index.js
+++ src/index.js
@@ -1,6 +1,10 @@
 import './style.css';//so it gets compile
-import {mainImgComponent,secImgComponent} from "./home.js" //to import components
+import {mainImgComponent,secImgComponent, secImgComponentLeft, secImgComponentRight} from "./home.js" //to import components
 
 
 document.getElementById('content').appendChild(mainImgComponent());
 document.getElementById('content').appendChild(secImgComponent());
+
+//append 2 divs into second-img div
+document.querySelector("second-img").appendChild(secImgComponentLeft());
+document.querySelector("second-img").appendChild(secImgComponentRight());
