63ac19f15fe6d25f8c3e419a69c846fc1b5061dc
Meissa <meissasy@gmail.com>
2024-08-05 13:13:27 -0400

    wrap components in function

--- src/index.js
+++ src/index.js
@@ -1,13 +1,15 @@
 import './style.css';//so it gets compile
 import {mainImgComponent,secImgComponent, secImgComponentLeft, secImgComponentRight, footer} from "./home.js" //to import components
 
-
-document.getElementById('content').appendChild(mainImgComponent());
-document.getElementById('content').appendChild(secImgComponent());
-
-//append 2 divs into second-img div
-document.querySelector(".second-img").appendChild(secImgComponentLeft());
-document.querySelector(".second-img").appendChild(secImgComponentRight());
+//all home components
+function selectHome(){
+    document.getElementById('content').appendChild(mainImgComponent());
+    document.getElementById('content').appendChild(secImgComponent());
+    
+    //append 2 divs into second-img div
+    document.querySelector(".second-img").appendChild(secImgComponentLeft());
+    document.querySelector(".second-img").appendChild(secImgComponentRight());
+}
 
 //append footer
 document.getElementById('content').appendChild(footer());
