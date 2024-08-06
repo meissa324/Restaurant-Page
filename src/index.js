f55e3d7f959ee7842f47d9bdcd248c7daea6cc17
Meissa <meissasy@gmail.com>
2024-08-06 18:12:55 -0400

    make a function to clear content of child elements

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import {formComponent,nameInputComponent,emailInputComponent,emailLabelComponent
 function selectHome(){
     document.getElementById('content').appendChild(mainImgComponent());
     document.getElementById('content').appendChild(secImgComponent());
-    
+
     //append 2 divs into second-img div
     document.querySelector(".second-img").appendChild(secImgComponentLeft());
     document.querySelector(".second-img").appendChild(secImgComponentRight());
@@ -32,5 +32,25 @@ function selectContact(){
     document.querySelector("form").appendChild(textAreaComponent());
 }
 
+//clears every element in #content
+function clearContent(){
+//loop to select each element and delete it
+
+//select parent
+let parentElement = document.getElementById('content');
+
+//have array of the children 
+let childNodes = Array.from(parentElement.children);
+
+//loop over array to remove each child
+for(const element of childNodes){ 
+    if(!element.classList.contains("footer")){//if its not the footer delete it
+        element.remove()
+    }
+    
+}
+
+//we can run it in console to make sure it works
+}
 //append footer
 document.getElementById('content').appendChild(footer());
